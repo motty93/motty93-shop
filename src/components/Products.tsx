60a84f8245d76d23cac5dfcf02c6569cbf57d55c
motty93 <rdwbocungelt5@gmail.com>
2022-06-06 15:47:11 +0900

    feat: div分離

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -2,10 +2,12 @@ import { NextPage } from 'next'
 import { Card } from './Card'
 
 export const Products: NextPage = () => (
-  <main className="flex gap-6 m-14">
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+  <main className="h-1/2 h-screen m-14">
+    <div className="md:flex justify-center md:gap-6 mx-auto">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
   </main>
 )
