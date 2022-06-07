8bdd32ddabcaea738e196fb6285970c8cddacb09
motty93 <rdwbocungelt5@gmail.com>
2022-06-07 14:36:07 +0900

    style: 中央寄せ調整

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -4,7 +4,7 @@ import { Card } from './Card'
 export const Products: NextPage = () => (
   <main className="h-1/2 md:h-full m-14">
     <h1 className="text-center font-bold text-2xl mb-6">出品してる商品</h1>
-    <div className="flex flex-wrap justify-center gap-4 mx-auto md:justify-start md:gap-6 ">
+    <div className="flex flex-wrap items-center justify-center md:space-x-4 md:gap-6">
       <Card />
       <Card />
       <Card />
