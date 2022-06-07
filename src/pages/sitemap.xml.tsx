a004c48dfbf983a3cedfa699c8aa8b29b53a537e
motty93 <rdwbocungelt5@gmail.com>
2022-06-07 17:48:59 +0900

    feat: feed sitemap

---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,18 @@
+import { GetServerSidePropsContext } from 'next'
+import { generateSitemapXml } from '@scripts'
+
+export const getServerSideProps = async ({ res }: GetServerSidePropsContext) => {
+  const xml = await generateSitemapXml()
+  res.statusCode = 200
+  // 24時間キャッシュ
+  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate')
+  res.setHeader('Content-Type', 'text/xml')
+  res.end(xml)
+
+  return {
+    props: {},
+  }
+}
+
+const Sitemap = () => null
+export default Sitemap
