f1bedcb4f1f9571fc1cd39919af2bcb78111c1cf
motty93 <rdwbocungelt5@gmail.com>
2022-06-07 12:14:49 +0900

    feat: products レスポンシブ対応

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from 'next'
 
 export const Card: NextPage = () => (
-  <div className="card w-auto bg-base-100 shadow-xl">
+  <div className="card w-auto bg-base-100 shadow-xl hover:cursor-pointer md:mb-4">
     <figure>
       <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" />
     </figure>
@@ -12,8 +12,8 @@ export const Card: NextPage = () => (
       </h2>
       <p>If a dog chews shoes whose shoes does he choose?</p>
       <div className="card-actions justify-end">
-        <div className="badge badge-outline">Fashion</div>
-        <div className="badge badge-outline">Products</div>
+        <div className="badge badge-outline hover:bg-sky-200">Fashion</div>
+        <div className="badge badge-outline hover:bg-sky-200">Products</div>
       </div>
     </div>
   </div>
