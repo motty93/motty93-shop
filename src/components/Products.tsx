239fbe12ba08fb68e87637af37f5ac62b4b0d748
motty93 <rdwbocungelt5@gmail.com>
2022-06-07 12:22:10 +0900

    feat: products title

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,6 +3,7 @@ import { Card } from './Card'
 
 export const Products: NextPage = () => (
   <main className="h-1/2 md:h-full m-14">
+    <h1 className="text-center font-bold text-2xl mb-6">出品してる商品</h1>
     <div className="flex flex-wrap justify-center gap-4 mx-auto md:flex-wrap md:justify-start md:gap-6 ">
       <Card />
       <Card />
