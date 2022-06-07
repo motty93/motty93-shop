b5e9cc89473161c2334e91f3949f306f134fea00
motty93 <rdwbocungelt5@gmail.com>
2022-06-07 17:49:06 +0900

    feat: エラーになるので一旦コメントアウト

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'microcms-js-sdk'
 import { config } from '@/site.config'
 
-export const client = createClient({
-  serviceDomain: config.serviceId,
-  apiKey: config.apiKey,
-})
+// export const client = createClient({
+//   serviceDomain: config.serviceId,
+//   apiKey: config.apiKey,
+// })
