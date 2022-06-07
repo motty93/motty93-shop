52ed9d0ac8800d13055ab6cef2787d6cd32fc46f
motty93 <rdwbocungelt5@gmail.com>
2022-06-07 15:43:20 +0900

    feat: return components

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,14 +1,16 @@
 import { NextPage } from 'next'
 import { Card } from './Card'
 
-export const Products: NextPage = () => (
-  <main className="h-1/2 md:h-full m-14">
-    <h1 className="text-center font-bold text-2xl mb-6">出品してる商品</h1>
-    <div className="flex flex-wrap items-center justify-center md:space-x-4 md:gap-6">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  </main>
-)
+export const Products: NextPage = () => {
+  return (
+    <main className="h-1/2 md:h-full m-14">
+      <h1 className="text-center font-bold text-2xl mb-6">出品してる商品</h1>
+      <div className="flex flex-wrap items-center justify-center md:space-x-4 md:gap-6">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </main>
+  )
+}
