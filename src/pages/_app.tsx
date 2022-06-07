1f2939c9159d3661ce01ab24e59a7f424f587cec
motty93 <rdwbocungelt5@gmail.com>
2022-06-07 17:47:04 +0900

    feat: recoil rootとかScriptとか

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,63 @@
+import { AppProps } from 'next/app'
+import Script from 'next/script'
+import Head from 'next/head'
+import { RecoilRoot } from 'recoil'
+
+import { config } from '@/site.config'
+import { GA_TRACKING_ID } from '@/utils'
 import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import { Footer, Hero } from '@/components'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const title = config.siteMeta.title
+  const pageUrl = config.baseUrl
+  const description = config.siteMeta.description
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content={pageUrl} />
+        <meta property="og:site" content={title} />
+        <meta name="description" content={description} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="color-scheme" content="light dark" />
+        <link rel="canonical" href={pageUrl} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+        }}
+      />
+      <Script
+        src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js"
+        integrity="sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ=="
+        crossOrigin="anonymous"
+      />
+      <div className="wrapper">
+        <Hero />
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </>
+  )
 }
 
-export default MyApp
+const MyAppWrapper = (props: AppProps) => (
+  <RecoilRoot>
+    <MyApp {...props} />
+  </RecoilRoot>
+)
+
+export default MyAppWrapper
