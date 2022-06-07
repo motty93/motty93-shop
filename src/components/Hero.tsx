98c48da3ec6b1a5211f8dcf551659ef74bc34f97
motty93 <rdwbocungelt5@gmail.com>
2022-06-07 16:07:10 +0900

    feat: image opecity

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export const Hero: NextPage = () => (
   <div className="hero min-h-screen">
     <div className="hero-overlay bg-opacity-80"></div>
-    <Image className="h-screen" src="/images/hero.jpg" layout="fill" objectFit="cover" />
+    <Image className="hero h-screen opacity-20" src="/images/hero.jpg" layout="fill" objectFit="cover" />
     <div className="hero-content text-center text-neutral-content">
       <div className="max-w-lg">
         <h1 className="mb-5 text-4xl md:text-5xl lg:text-6xl font-bold">The shop's もてぃ</h1>
