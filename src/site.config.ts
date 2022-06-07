1776dc3fb5fb26f40f0e7fa4442219b949f81ed3
motty93 <rdwbocungelt5@gmail.com>
2022-06-07 17:46:03 +0900

    feat: site config

---
+++ src/site.config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  siteMeta: {
+    title: "The shop's もてぃ",
+    description: 'いらないものを販売するもてぃ専用サイト',
+  },
+  baseUrl:
+    process.env.NODE_ENV === 'production'
+      ? process.env.NEXT_PUBLIC_BASEURL
+      : 'http://localhost:3000',
+  baseDraftUrl:
+    process.env.NODE_ENV === 'production'
+      ? process.env.NEXT_PUBLIC_DRAFT_BASEURL
+      : 'http://localhost:3000',
+  apiKey: process.env.MICROCMS_APIKEY || '',
+  serviceId: process.env.SERVICE_ID || '',
+  headerLinks: [],
+  defaultLimit: process.env.NEXT_PUBLIC_DEFAULT_LIMIT ?? '10',
+  defaultMaxLimit: 50,
+}
