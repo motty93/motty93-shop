d8a7750458c4eb3af37ded32c2e75ad635e2ee32
motty93 <rdwbocungelt5@gmail.com>
2022-06-08 14:54:20 +0900

    feat: microcmsの型作成

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -1,16 +1,6 @@
-import {
-  MicroCMSListContent,
-  MicroCMSListResponse,
-  MicroCMSQueries,
-} from 'microcms-js-sdk'
+import { MicroCMSListContent, MicroCMSListResponse, MicroCMSQueries } from 'microcms-js-sdk'
 
 export type Queries = MicroCMSQueries
-
-export type TocTypes = {
-  text: string
-  id: string
-  name: string
-}
 
 export type BodyEntity = {
   fieldId: 'richEditor' | 'html' | 'markdown'
@@ -19,22 +9,17 @@ export type BodyEntity = {
   markdown?: string
 }
 
-export interface IBlog extends ContentBase {
-  title?: string
-  category?: ICategory
-  tag?: ITag[]
-  toc_visible?: boolean
-  body?: BodyEntity[]
-  description?: string
-  ogimage?: IMicroCmsImageType
-  related_blogs: IBlog[]
+export interface IProduct extends MicroCMSListContent {
+  ogimage: IMicroCmsImageType
+  title: string
+  description: string
+  body: BodyEntity
+  status: string
+  price: number
+  categories: ICategory[]
 }
 
-export interface ICategory extends ContentBase {
-  name?: string
-}
-
-export interface ITag extends ContentBase {
+export interface ICategory extends MicroCMSListContent {
   name?: string
 }
 
@@ -48,11 +33,11 @@ export interface IMicroCmsImageType {
   width: number
 }
 
-export interface IDraftResponse {
-  blog: IBlog
-  toc: TocTypes[]
-  body: string
-}
+// export interface IDraftResponse {
+//   blog: IBlog
+//   toc: TocTypes[]
+//   body: string
+// }
 
 export type Response<T = any> = {
   data: T
