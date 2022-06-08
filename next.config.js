7632413398cb6dd18f72b3dea2d8dd1153d95bd1
motty93 <rdwbocungelt5@gmail.com>
2022-06-08 14:54:29 +0900

    feat: nextconfig

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   webpack: (config, { isServer }) => {
-    if(!isServer) {
+    if (!isServer) {
       config.resolve.fallback.fs = false
     }
 
     return config
-  }
+  },
 }
 
 module.exports = nextConfig
