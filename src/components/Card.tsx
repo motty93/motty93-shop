f09ba52c77e99d6aaa7aea20d3427fc4b77ab1e3
motty93 <rdwbocungelt5@gmail.com>
2022-06-09 12:54:11 +0900

    style: サイズ調整

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,17 +6,17 @@ type Props = {
 }
 
 export const Card: NextPage<Props> = ({ product }) => (
-  <div className="w-96 shadow-xl hover:shadow-2xl hover:cursor-pointer mb-4 card card-compact bg-base-100 active:bg-base-200">
+  <div className="md:w-96 md:h-auto shadow-xl hover:shadow-2xl hover:cursor-pointer mb-4 card card-compact bg-base-100 active:bg-base-200">
     <figure>
-      <img src={`${product.ogimage.url}?w=400&h=225`} alt={product.brand} />
+      <img className="w-32 md:w-64 h-28 md:h-auto" src={`${product.ogimage.url}`} alt={product.brand} />
     </figure>
     <div className="card-body">
-      <h2 className="card-title">{product.title}</h2>
-      <div className="badge badge-secondary">{product.condition}</div>
-      <p className="whitespace-pre-wrap">{product.description}</p>
+      <h2 className="card-title text-sm font-bold md:text-xl">{product.title}</h2>
+      <div className="badge badge-secondary text-xs md:text-sm">{product.condition}</div>
+      <p className="whitespace-pre-wrap text-xs md:text-base">{product.description}</p>
       <div className="justify-end card-actions">
         {product.categories.map((category) => (
-          <div className="hover:bg-sky-200 badge badge-primary" key={category.id}>
+          <div className="hover:bg-sky-200 badge badge-primary text-xs" key={category.id}>
             {category.name}
           </div>
         ))}
