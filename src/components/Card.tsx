ae1059cf2f8a5c36ba58030523e14348dbf7b7d4
motty93 <rdwbocungelt5@gmail.com>
2022-06-09 14:36:43 +0900

    feat: productIdでdynamic routing

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,29 @@
 import { IProduct } from '@/types'
 import { NextPage } from 'next'
+import Link from 'next/link'
 
 type Props = {
   product: IProduct
 }
 
 export const Card: NextPage<Props> = ({ product }) => (
-  <div className="w-64 md:w-96 md:h-auto shadow-xl hover:shadow-2xl hover:cursor-pointer mb-4 card card-compact bg-base-100 active:bg-base-200">
-    <figure className="pt-3 md:pt-6">
-      <img className="w-32 md:w-64 h-28 md:h-auto" src={`${product.ogimage.url}`} alt={product.brand} />
-    </figure>
-    <div className="card-body">
-      <h2 className="card-title text-sm font-bold md:text-xl">{product.title}</h2>
-      <div className="badge badge-secondary text-xs md:text-sm">{product.condition}</div>
-      <p className="whitespace-pre-wrap text-xs md:text-base">{product.description}</p>
-      <div className="justify-end card-actions">
-        {product.categories.map((category) => (
-          <div className="hover:bg-sky-200 badge badge-primary text-xs" key={category.id}>
-            {category.name}
-          </div>
-        ))}
+  <Link href="/[productId]" as={`/${product.id}`}>
+    <a className="w-64 md:w-96 md:h-auto shadow-xl hover:shadow-2xl hover:cursor-pointer mb-4 card card-compact bg-base-100 active:bg-base-200">
+      <figure className="pt-3 md:pt-6">
+        <img className="w-32 md:w-64 h-28 md:h-auto" src={`${product.ogimage.url}`} alt={product.brand} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-sm font-bold md:text-xl">{product.title}</h2>
+        <div className="badge badge-secondary text-xs md:text-sm">{product.condition}</div>
+        <p className="whitespace-pre-wrap text-xs md:text-base">{product.description}</p>
+        <div className="justify-end card-actions">
+          {product.categories.map((category) => (
+            <div className="hover:bg-sky-200 badge badge-primary text-xs" key={category.id}>
+              {category.name}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  </div>
+    </a>
+  </Link>
 )
---
+++ src/pages/[productId].tsx
@@ -0,0 +1,37 @@
+import { getAllProducts } from '@/utils'
+import { GetStaticPropsContext, NextPage } from 'next'
+import { useRouter } from 'next/router'
+
+const Product: NextPage = () => {
+  const router = useRouter()
+  if (router.isFallback) {
+    return <>loading</>
+  }
+
+  return <>hello</>
+}
+
+export const getStaticPaths = async () => {
+  const data = await getAllProducts()
+  const ids = data.contents.map((product) => {
+    return { params: { productId: product.id } }
+  })
+
+  return {
+    paths: ids,
+    fallback: false, //上記以外のパスでアクセスすると404
+  }
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const productId: any = context.params?.productId || '1'
+  const product = { id: productId }
+
+  return {
+    props: {
+      product
+    }
+  }
+}
+
+export default Product
