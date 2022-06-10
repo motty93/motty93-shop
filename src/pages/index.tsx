81030238519b9449e49ce00dd13256a024e8a894
motty93 <rdwbocungelt5@gmail.com>
2022-06-10 09:13:53 +0900

    style: font size調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage<Props> = ({ products }) => (
   <>
     <Hero />
     <main className="m-14 h-1/2 md:h-full">
-      <h1 className="mb-6 text-2xl font-bold text-center">出品してる商品</h1>
+      <h1 className="mb-6 text-xl md:text-2xl font-bold text-center">出品してる商品</h1>
       <div className="flex flex-wrap justify-center items-center gap-7">
         {products.map((product) => (
           <Card product={product} key={product.id} />
