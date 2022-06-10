a4db0096d775c63ab6d7cf52b08c297484ee27b2
motty93 <rdwbocungelt5@gmail.com>
2022-06-10 09:13:45 +0900

    feat: sitemap

--- src/scripts/SitemapUtil.ts
+++ src/scripts/SitemapUtil.ts
@@ -1,24 +1,25 @@
-// import { config } from '@/site.config'
-// import { formatDate } from '../utils/DateUtil'
-// import { getAllBlogs } from './Blog'
+import { config } from '@/site.config'
+import { getAllProducts } from '@/utils'
+import { formatDate } from '../utils/DateUtil'
 
-// export const generateSitemapXml = async (): Promise<string> => {
-//   let xml: string = `<?xml version="1.0" encoding="UTF-8"?>`
-//   xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`
-//
-//   const blogs = await getAllBlogs()
-//   if (!blogs.contents) { return xml }
-//
-//   blogs.contents.forEach((blog) => {
-//     xml += `<url>
-//         <loc>${config.baseUrl}/${blog.id}</loc>
-//         <lastmod>${formatDate(blog.createdAt, 'YYYY-MM-DD')}</lastmod>
-//         <changefreq>weekly</changefreq>
-//       </url>`
-//   })
-//
-//   xml += `</urlset>`
-//
-//   return xml
-// }
-export const generateSitemapXml = async () => {}
+export const generateSitemapXml = async (): Promise<string> => {
+  let xml = `<?xml version="1.0" encoding="UTF-8"?>`
+  xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`
+
+  const products = await getAllProducts()
+  if (!products.contents) {
+    return xml
+  }
+
+  products.contents.forEach((product) => {
+    xml += `<url>
+        <loc>${config.baseUrl}/${product.id}</loc>
+        <lastmod>${formatDate(product.createdAt, 'YYYY-MM-DD')}</lastmod>
+        <changefreq>weekly</changefreq>
+      </url>`
+  })
+
+  xml += `</urlset>`
+
+  return xml
+}
